2978b3e8a85b96f5328152ada1b140b16f90f68c
Andrey Gaivoronsky <onetothreecreator@mailfence.com>
2025-12-17 19:34:57 +0500

    Better potentiometer rounding (at the cost of input latency, partially mitigated by MAX_DELTA)

--- InputAverager.cpp
+++ InputAverager.cpp
@@ -3,12 +3,35 @@
 #include "InputAverager.hpp"
 
 uint16_t InputAverager::get() {
-  uint16_t sum=0;
-  for(uint8_t i = 0; i < ANALOG_INPUT_AVERAGE_FACTOR - 1; ++i) {
+  uint32_t sum = ANALOG_INPUT_AVERAGE_NEW / 2;
+  uint16_t input;
+  for (uint8_t i = 0; i < ANALOG_INPUT_AVERAGE_NEW; ++i) {
+    sum += analogRead(pin);
+  }
+  input = sum * range / 1024 / ANALOG_INPUT_AVERAGE_NEW;
+  sum = avg[0] + ANALOG_INPUT_AVERAGE_HISTORY / 2;
+  for(uint8_t i = 0; i < ANALOG_INPUT_AVERAGE_HISTORY - 2; ++i) {
     avg[i] = avg[i + 1];
     sum += avg[i];
   }
-  avg[ANALOG_INPUT_AVERAGE_FACTOR - 1] = analogRead(pin);
-  sum += avg[ANALOG_INPUT_AVERAGE_FACTOR - 1];
-  return (sum + (ANALOG_INPUT_AVERAGE_FACTOR/2)) / ANALOG_INPUT_AVERAGE_FACTOR;
+  
+  avg[ANALOG_INPUT_AVERAGE_HISTORY - 2] = input;
+  sum += avg[ANALOG_INPUT_AVERAGE_HISTORY - 2];
+  uint16_t result = sum / ANALOG_INPUT_AVERAGE_HISTORY;
+  repeat_counter = min(repeat_counter + (result == last) * 2 - 1, (repeat_counter & 0x80) + ANALOG_INPUT_MIN_REPEATS);
+  repeat_counter |= (repeat_counter >= ANALOG_INPUT_MIN_REPEATS - 1) << 7;
+  if (repeat_counter > 0x80) {
+    result = last;
+  } else {
+    repeat_counter &= ~0x80;
+  }
+  if (abs((int16_t)result - (int16_t)input) > ANALOG_INPUT_MAX_DELTA) {
+    for(uint8_t i = 0; i < ANALOG_INPUT_AVERAGE_HISTORY - 1; ++i) {
+      avg[i] = input;
+    }
+    result = input;
+    repeat_counter = 0;
+  }
+  last = result;
+  return result;
 }
--- InputAverager.hpp
+++ InputAverager.hpp
@@ -1,13 +1,19 @@
 #ifndef INPUT_AVERAGER_H
 #define INPUT_AVERAGER_H
-#define ANALOG_INPUT_AVERAGE_FACTOR 10
+#define ANALOG_INPUT_AVERAGE_HISTORY 20
+#define ANALOG_INPUT_AVERAGE_NEW 10
+#define ANALOG_INPUT_MAX_DELTA 25
+#define ANALOG_INPUT_MIN_REPEATS 25
 #include <inttypes.h>
 class InputAverager {
 private:
-  uint16_t avg[ANALOG_INPUT_AVERAGE_FACTOR];
+  uint16_t avg[ANALOG_INPUT_AVERAGE_HISTORY - 1];
+  uint16_t range;
+  uint16_t last;
   uint8_t pin;
+  uint8_t repeat_counter;
 public:
-  inline InputAverager(uint8_t pin): pin(pin), avg{} {};
+  inline InputAverager(uint8_t pin, uint16_t map_max_range=1024): pin(pin), range{map_max_range}, avg{}, last{}, repeat_counter{} {};
   uint16_t get();
 };
 #endif // INPUT_AVERAGER_H
